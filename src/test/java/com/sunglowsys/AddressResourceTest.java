062e866bcef7f2e94dd7e52f556e1db202208907
Ranjit10496 <ranjit.kumar96@outlook.com>
2022-11-03 15:23:53 +0530

    done

--- src/test/java/com/sunglowsys/AddressResourceTest.java
+++ src/test/java/com/sunglowsys/AddressResourceTest.java
@@ -9,16 +9,21 @@ import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -54,6 +59,17 @@ public class AddressResourceTest {
         address.setState("New Delhi");
         address.setCountry("India");
         address.setZipcode("204211");
+
+        return address;
+    }
+    private static Address createAddress2() {
+        Address address = new Address();
+        address.setAddressLine1("Kasganj");
+        address.setAddressLine2("Mohanpura");
+        address.setCity("Aligarh");
+        address.setState("Uttar Pradesh");
+        address.setCity("India");
+        address.setZipcode("204111");
 
         return address;
     }
@@ -95,6 +111,37 @@ public class AddressResourceTest {
                         .andReturn();
         assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
         assertEquals(EXPEXTED_RESULT, result.getResponse().getContentAsString(),JSONCompareMode.LENIENT);
+
+    }
+    @Test
+    void getAllAddressTest() throws Exception {
+        Address mockAddress1 = createAddress();
+        mockAddress1.setId(1l);
+        Address mockAddress2 = createAddress2();
+        mockAddress2.setId(2l);
+        List<Address> addressList = new ArrayList<>();
+        addressList.add(mockAddress1);
+        addressList.add(mockAddress2);
+
+        when(addressService.findAll(any())).thenReturn(new PageImpl<>(addressList, PageRequest.of(0,2),2));
+        MvcResult result = mockMvc.perform(get("/api/addresses")
+                .contentType(TestUtil.APPLICATION_JSON)
+                .content(TestUtil.convertObjectToJsonBytes(addressList)))
+                .andReturn();
+        assertEquals(HttpStatus.OK.value(), result.getResponse().getStatus());
+       // assertEquals(EXPEXTED_RESULT,result.getResponse().getContentAsString(),JSONCompareMode.LENIENT);
+    }
+    @Test
+    void deleteAddressTest() throws Exception {
+        Address mockAddress = createAddress();
+        mockAddress.setId(1l);
+
+        doNothing().when(addressService).delete(anyLong());
+        MvcResult result = mockMvc.perform(delete("/api/addresses/1")
+                .contentType(TestUtil.APPLICATION_JSON)
+                .content(TestUtil.convertObjectToJsonBytes(mockAddress)))
+                .andReturn();
+        assertEquals(HttpStatus.OK.value(),result.getResponse().getStatus());
 
     }
 
